0e8b924f2c15e75cbadee1666a1200d05a0b24d1
Ashritha Madabhushana <78677306+m-ashritha@users.noreply.github.com>
2021-09-18 19:31:53 +0530

    Add files via upload

---
+++ Triangle_pattern.java
@@ -0,0 +1,24 @@
+/* print the following pattern
+	1
+	2 2
+	3 3 3
+	4 4 4 4
+	5 5 5 5 5
+*/
+
+class Triangle_pattern
+{
+	public static void main(String args[])
+	{
+		int i,j;
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+		
