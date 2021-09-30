9a2123f50eed23d7ffa8b60e06e8afdad38bc370
Ashritha Madabhushana <78677306+m-ashritha@users.noreply.github.com>
2021-09-30 10:02:48 +0530

    Add files via upload

---
+++ Triangle_Patterns/Triangle_pattern_2.c
@@ -0,0 +1,27 @@
+/*print the following pattern 
+	    1
+	   2 2
+	  3 3 3
+	 4 4 4 4 
+	5 5 5 5 5 		
+*/
+
+// C Code
+
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5-i;j++)
+		{
+			printf(" ");
+		}
+		for(k=j;k<=5;k++)
+		{
+			printf("%d ",i);
+		}
+		printf("\n");
+	}
+}
