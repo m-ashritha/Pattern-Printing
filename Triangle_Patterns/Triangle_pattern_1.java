52b474a765e0f82b4912d82819ae985e9cd9d414
Ashritha Madabhushana <78677306+m-ashritha@users.noreply.github.com>
2021-09-30 09:42:39 +0530

    Update Triangle_pattern_1.java

--- Triangle_Patterns/Triangle_pattern_1.java
+++ Triangle_Patterns/Triangle_pattern_1.java
@@ -1,1 +1,26 @@
 
+/* print the following pattern
+	1
+	2 2
+	3 3 3
+	4 4 4 4
+	5 5 5 5 5
+*/
+
+//java Code
+
+class Triangle_pattern
+{
+    public static void main(String args[])
+    {
+          int i,j;
+          for(i=1;i<=5;i++)
+          {
+                for(j=1;j<=i;j++)
+                {
+                  System.out.print(i+" ");
+                }
+            System.out.println();
+          }
+    }
+}
