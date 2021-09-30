a9b00b674dd261f2ceb236cfa4acd55772272326
Ashritha Madabhushana <78677306+m-ashritha@users.noreply.github.com>
2021-09-30 10:06:18 +0530

    Create Triangle_pattern_2.c

---
+++ Triangle_Patterns/Triangle_pattern_2.c
@@ -0,0 +1,30 @@
+/* print the following pattern
+			      1 
+		      2 2 
+		    3 3 3 
+		  4 4 4 4 
+		5 5 5 5 5 
+*/
+
+// C Code
+
+#include<stdio.h>
+int main()
+{
+	    int i,j,k;
+      for(i=1;i<=5;i++)
+      {
+        //for each line prints (5-i)*2 spaces
+              for(j=1;j<=5-i;j++)
+              {
+                printf("  ");
+              }
+              //for each line prints i value i times
+              for(k=j;k<=5;k++)
+              {
+                printf("%d ",i);
+              }
+              printf("\n");
+      }
+	    return 0;
+}
